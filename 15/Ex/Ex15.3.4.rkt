949b01be75c7514f3889e20ef26f4514651813cc
abyte <gechanghang@gmail.com>
2016-04-26 00:54:53 +0800

    Test git

--- 15/Ex/Ex15.3.4.rkt
+++ 15/Ex/Ex15.3.4.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname Ex15.3.4) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.rkt" "teachpack" "htdp")))))
+;;Test Git
 ;;Develop the program find. The function consumes a Web page and a symbol. 
 ;;It produces false, if the symbol does not occur in the body of the page or its embedded Web pages. 
 ;;If the symbol occurs at least once, it produces a list of the headers that are encountered on the way to the symbol.
